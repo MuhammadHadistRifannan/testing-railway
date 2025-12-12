86a36088e4027767894a9c73b3bb822e6728cbcf
MuhammadHadistRifannan <ipanjabar933@gmail.com>
2025-12-13 03:03:11 +0700

    add key

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 
 var app = builder.Build();
 
@@ -12,9 +16,11 @@ app.UseHttpsRedirection();
 
 app.MapGet("/" , () =>
 {
+    var key = Environment.GetEnvironmentVariable("APIKEY");
     return Results.Ok(new
     {
-        text = "Ini adalah webapi"
+        text = "Ini adalah webapi" ,
+        apikey = key ?? "Tidak ditemukan Key"
     });
 });
 
